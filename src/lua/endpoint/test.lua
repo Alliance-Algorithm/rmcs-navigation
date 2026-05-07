a2fcc76bd8e590b541fae43570148d6a1124ba38
gqsdjhh <1369103595@qq.com>
2026-05-07 23:30:12 +0800

    temp

--- src/lua/endpoint/test.lua
+++ src/lua/endpoint/test.lua
@@ -19,6 +19,9 @@ local restart_navigation = function()
 		launch_odin1 = false,
 		use_sim_time = false,
 	}
+	-- action:exchange_17mm_bullet(100)
+	action:confirm_revive();
+	-- action:switch_mode(1);
 end
 
 ---
@@ -36,12 +39,14 @@ on_init = function()
 	action:switch_topic_forward(true)
 
 	restart_navigation()
-	-- edges:on(blackboard.getter.rswitch, "UP", restart_navigation)
+	edges:on(blackboard.getter.rswitch, "UP", restart_navigation)
 
 	scheduler:append_task(function()
 		while true do
 			request:sleep(1)
 			-- action:info("limit: " .. blackboard.user.chassis_power_limit)
+			action:info(string.format("bullet %d health %d mode %d can_confirm_free_revive %s gimbal_commmand_keyboad %d x %d y %d",
+			blackboard.user.bullet, blackboard.user.health, blackboard.game.sentry_mode, blackboard.game.can_confirm_free_revive, blackboard.map_command.keyboard, blackboard.map_command.x, blackboard.map_command.y))
 		end
 	end)
 end
@@ -59,5 +64,11 @@ end
 
 --- 由 NAV2 发布的目标速度值，在此处理回调
 on_control = function(x, y, _)
+	local now = clock:now()
+	if last_control_log_time == nil or now - last_control_log_time >= 2.0 then
+		action:info(string.format("NAV2 目标速度: vx=%.3f, vy=%.3f", x, y))
+		last_control_log_time = now
+	end
+
 	action:update_chassis_vel(x, y)
 end
