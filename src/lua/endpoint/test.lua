cd44984f494080d07aaaf0a3a272db112fb84887
zlq04222 <1542498005@qq.com>
2026-07-23 00:11:18 +0800

    chore: simplify test endpoint enable-control and gimbal setup

--- src/lua/endpoint/test.lua
+++ src/lua/endpoint/test.lua
@@ -37,7 +37,6 @@ on_init = function()
 	clock:reset(blackboard.meta.timestamp)
 
 	action:bind(scheduler)
-	action:gimbal_toward(0 / 0, 0 / 0)
 
 	action:info(ascii.banner)
 	action:warn("⚠️ TEST 模式，别上场哦")
@@ -52,49 +51,64 @@ on_init = function()
 		action:switch_topic_forward(true)
 	end
 
+	local restart_navigation = function()
+		action:info("导航即将重启")
+		action:restart_navigation {
+			global_map = "rmuc",
+			launch_livox = true,
+			launch_odin1 = false,
+			use_sim_time = false,
+		}
+	end
 	restart_navigation()
 
-	action:set_gimbal_yt(10)
-	action:set_gimbal_pt(5)
-	action:gimbal_scan(-0.4, 0)
+	-- action:gimbal_scan(-0.2, 0.2)
+	-- action:set_gimbal_yt(20)
+	-- action:set_gimbal_pt(10)
+
 
 	scheduler:append_task(task.robot_status)
+	-- scheduler:append_task(function()
+	-- local switch_order = order.new(blackboard.getter.rswitch, 0.5)
+	-- local posture_index = 0
+	-- local events = {
+	-- SentryEvent.SWITCH_POSE_ATTACK,
+	-- SentryEvent.SWITCH_POSE_DEFENSE,
+	-- SentryEvent.SWITCH_POSE_MOVE,
+	-- SentryEvent.SWITCH_POSE_POWERED_ATTACK,
+	-- SentryEvent.SWITCH_POSE_POWERED_DEFENSE,
+	-- SentryEvent.SWITCH_POSE_POWERED_MOVE,
+	-- }
+	-- local event_names = { "ATTACK", "DEFENSE", "MOVE", "POWERED_ATTACK", "POWERED_DEFENSE", "POWERED_MOVE" }
+	--
+	-- switch_order:on({ "MIDDLE", "UP", "MIDDLE" }, function()
+	-- action:info("中上中触发: 重启导航")
+	-- restart_navigation()
+	-- request:sleep(3)
+	-- action:gimbal_nod(1)
+	-- end)
+	--
+	-- switch_order:on({ "MIDDLE", "DOWN", "MIDDLE" }, function()
+	-- posture_index = (posture_index % #events) + 1
+	-- local ev = events[posture_index]
+	-- action:info("中下中触发: push " .. event_names[posture_index])
+	-- action:push_sentry_event(ev)
+	-- end)
+	--
+	-- while true do
+	-- action:switch_navigation(blackboard.play.rswitch == "UP")
+	-- switch_order:spin()
+	-- request:yield()
+	-- end
+	-- end)
+
+
+	action:set_gimbal_yt(2)
+	action:set_gimbal_pt(2)
+	action:gimbal_scan(0, 0)
 	scheduler:append_task(function()
-		local switch_order = order.new(blackboard.getter.rswitch, 0.5)
-		local posture_index = 0
-		local events = {
-			SentryEvent.SWITCH_POSE_ATTACK,
-			SentryEvent.SWITCH_POSE_DEFENSE,
-			SentryEvent.SWITCH_POSE_MOVE,
-			SentryEvent.SWITCH_POSE_POWERED_ATTACK,
-			SentryEvent.SWITCH_POSE_POWERED_DEFENSE,
-			SentryEvent.SWITCH_POSE_POWERED_MOVE,
-		}
-		local event_names = { "ATTACK", "DEFENSE", "MOVE", "POWERED_ATTACK", "POWERED_DEFENSE", "POWERED_MOVE" }
-
-		switch_order:on({ "MIDDLE", "UP", "MIDDLE" }, function()
-			action:info("中上中触发: 重启导航")
-			restart_navigation()
-			request:sleep(3)
-			action:gimbal_nod(1)
-		end)
-
-		switch_order:on({ "MIDDLE", "DOWN", "MIDDLE" }, function()
-			posture_index = (posture_index % #events) + 1
-			local ev = events[posture_index]
-			action:info("中下中触发: push " .. event_names[posture_index])
-			action:push_sentry_event(ev)
-		end)
-
 		while true do
-			action:switch_navigation(blackboard.play.rswitch == "UP")
-			switch_order:spin()
-			request:yield()
-		end
-	end)
-
-	scheduler:append_task(function()
-		while true do
+			action:update_enable_control(blackboard.play.rswitch == "UP")
 			request:sleep(0.1)
 		end
 	end)
